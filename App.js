42ba7ef3765a9c7b4278d4f5521635c5cc8c1137
s0461117 <as120450@gmail.com>
2018-01-07 03:30:01 +0800

    commit

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import React from 'react';
-
-import {
-    StyleSheet,
-    Platform,
-    Image,
-    Text,
-    View,
-    ListView,
-    Button,
-    TouchableOpacity,
-    TouchableHighlight,
-    AppRegistry,
-} from 'react-native';
-
-import Home from './page/home';
-import Pickertext from './page/pickertext';
-import Menu from './page/menu';
-import Listitem from './page/listitem';
-import Itembtn from './page/itembtn';
-import Checkout from './page/checkout';
-
-import {Router,Stack,Scene} from 'react-native-router-flux';
-
+import { StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  ListView,
+  Button,
+  TouchableOpacity,
+  Picker
+        } from 'react-native';
 
 import firebase from 'react-native-firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDhs2knntClbJYgLmd8Y3uq_oOiWfR8WQI",
+  apiKey: "apiKey",
   authDomain: "fireb-c693f.firebaseapp.com",
   databaseURL: "https://fireb-c693f.firebaseio.com",
   storageBucket: "fireb-c693f.appspot.com",
@@ -35,21 +22,6 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class App extends React.Component {
-
-
-  render() {
-    return (
-      <Router>
-              <Stack key="root">
-                <Scene key="home" component={Home} hideNavBar={true}/>
-                <Scene key="pickertext" component={Pickertext} title="點餐"/>
-                <Scene key="menu" component={Menu} title="選擇菜色"/>
-                <Scene key="listitem" component={Listitem} title="123"/>
-                <Scene key="itembtn" component={Itembtn} title="細項"/>
-                <Scene key="checkout" component={Checkout} title="結帳"/>
-
-              </Stack>
-            </Router>
 
     listenForItems(itemsRef) {
         itemsRef.on('value', (snap) => {
@@ -102,15 +74,11 @@ export default class App extends React.Component {
         </View>
        </TouchableOpacity>
 
-
     );
   }
 }
 
 const styles = StyleSheet.create({
-
-
-
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -168,7 +136,4 @@ const styles = StyleSheet.create({
     height:60,
     marginTop: 350,
   }
-
 });
-
-AppRegistry.registerComponent('Restaurant', () => Restaurant);
